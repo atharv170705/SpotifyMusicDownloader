096201680e0c7bc941ce9757ac9ad581cba15d27
Atharv Tambe <atharv170705@gmail.com>
2026-06-16 09:40:10 +0530

    perf: parallelize Spotify track fetches using Promise.all

--- backend/src/controllers/spotifyController.js
+++ backend/src/controllers/spotifyController.js
@@ -9,20 +9,23 @@ const getPlaylist = async (req, res) => {
         const playlistImage = data.coverArt.sources[0].url;
         const trackList = data.trackList;
         
-        const tracks = [];
-        for (const track of trackList) {
-            const trackId = track.uri.split(":")[2];
-            const trackUrl = `https://open.spotify.com/track/${trackId}`;
-            const trackData = await getData(trackUrl);
-            tracks.push({
-                id: trackId,
-                name: trackData.name,
-                artists: trackData.artists
-                    .map(artist => artist.name)
-                    .join(", "),
-                image: trackData.visualIdentity.image.at(-1)?.url,
-            });
-        }
+        const tracks = await Promise.all(
+            trackList.map(async (track) => {
+                const trackId = track.uri.split(":")[2];
+                const trackUrl = `https://open.spotify.com/track/${trackId}`;
+
+                const trackData = await getData(trackUrl);
+
+                return {
+                    id: trackId,
+                    name: trackData.name,
+                    artists: trackData.artists
+                        .map(a => a.name)
+                        .join(", "),
+                    image: trackData.visualIdentity.image.at(-1)?.url,
+                };
+            })
+        );
         res.status(200).json({
             playlist: {
                 name,
